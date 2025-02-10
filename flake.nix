e22757d41c3251877d7358f1a37725a925357dcd
FallenGoddess7 <69827634+FallenGoddess7@users.noreply.github.com>
2025-02-10 09:38:03 +0000

    edits

--- flake.nix
+++ flake.nix
@@ -4,11 +4,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
-    # Prevents home-manager from desyncronizing with nix
+    # Prevents desyncronizing with nixpkgs
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    # TODO: Add disko, impermanance, stylix, sops-nix, and NVF(or NixVim)
   };
 
-  outputs = { ... }@inputs:{
+  outputs = { ... }@inputs: let 
+  # Boilerplate-reducing functions and helpful tools.
+  myLibs = import ./libs/default.nix {inherit inputs;};
+  in with myLibs; {
+    # ============================================================ #
+    # =================== NixOS Configurations =================== #
     nixosConfigurations = {
       okabe = mkSystem ./hosts/okabe/configuration.nix; # Desktop #
 
@@ -17,6 +23,12 @@
       # TODO: implement an iso for the configuration of Yubikey's and GPG keys.
       # yubicoiso = mkSystem ./hosts/yubicoiso/configuration.nix;
     };
+    # ================ Home-manager Configurations =============== #
+    homeConfigurations = {
 
+    };
+
+    nixosModules.default = ./modules/default.nix;
+    homeManagerModules.default = ./home/default.nix;
   };
 }
