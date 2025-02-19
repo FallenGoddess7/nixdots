9f4c864ac419e510408acef74790927dd300be5a
FallenGoddess7 <69827634+FallenGoddess7@users.noreply.github.com>
2025-02-19 03:22:54 -0600

    edits

--- flake.nix
+++ flake.nix
@@ -1,5 +1,48 @@
+# ============================================================================ #
+# =               ######## ##          ###    ##    ## ########              = #
+# =               ##       ##         ## ##   ##   ##  ##                    = #
+# =               ##       ##        ##   ##  ##  ##   ##                    = #
+# =               ######   ##       ##     ## #####    ######                = #
+# =               ##       ##       ######### ##  ##   ##                    = #
+# =               ##       ##       ##     ## ##   ##  ##                    = #
+# =               ##       ######## ##     ## ##    ## ########              = #
+# ============================================================================ #
 {
   description = "My (Alice) configuration of NixOS via Flakes";
+
+  outputs = inputs@{ self, ... }: 
+  let
+    # ================================ System ================================ #
+    systemSettings = {
+      system = "x86_64";
+      timezone = "America/Chicago";
+      locale = "en_US.UTF-8";
+    };
+    
+    # ================================= User ================================= #
+    userSettings = {
+      username = "alice";
+      name = "Alice";
+      email = "Fallengoddess777+github@gmail.com";
+      theme = "everforest";
+    };
+
+    # ================================= Libs ================================= #
+    myLibs = import ./libs/default.nix {inherit inputs;};
+  in with myLibs; {
+    # ========================= NixOS Configurations ========================= #
+    nixosConfigurations = {
+      okabe = mkSystem ./hosts/okabe/configuration.nix; # Desktop #
+      # TODO: implement an iso for installaion of nixos.
+      # liveiso = mkSystem ./hosts/liveiso/configuration.nix;
+      # TODO: implement an iso for the configuration of Yubikey's and GPG keys.
+      # yubicoiso = mkSystem ./hosts/yubicoiso/configuration.nix;
+    };
+    # ====================== Home-manager Configurations ===================== #
+    homeConfigurations = {
+      # TODO: add home-manager configurations.
+    };
+  };
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -7,28 +50,5 @@
     # Prevents desyncronizing with nixpkgs
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     # TODO: Add disko, impermanance, stylix, sops-nix, and NVF(or NixVim)
-  };
-
-  outputs = { ... }@inputs: let 
-  # Boilerplate-reducing functions and helpful tools.
-  myLibs = import ./libs/default.nix {inherit inputs;};
-  in with myLibs; {
-    # ============================================================ #
-    # =================== NixOS Configurations =================== #
-    nixosConfigurations = {
-      okabe = mkSystem ./hosts/okabe/configuration.nix; # Desktop #
-
-      # TODO: implement an iso for installaion of nixos.
-      # liveiso = mkSystem ./hosts/liveiso/configuration.nix;
-      # TODO: implement an iso for the configuration of Yubikey's and GPG keys.
-      # yubicoiso = mkSystem ./hosts/yubicoiso/configuration.nix;
-    };
-    # ================ Home-manager Configurations =============== #
-    homeConfigurations = {
-      # TODO: add home-manager configurations.
-    };
-
-    nixosModules.default = ./modules/default.nix;
-    homeManagerModules.default = ./home/default.nix;
   };
 }
