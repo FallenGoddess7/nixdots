2156186a96aecbce9ab29636583b0bd315591166
FallenGoddess7 <69827634+FallenGoddess7@users.noreply.github.com>
2025-02-25 05:19:30 -0600

    edits

---
+++ home/common/default.nix
@@ -0,0 +1,9 @@
+{ inputs, pkgs, lib, ... }:
+{
+  imports = [
+    ./stylix.nix
+  ];
+
+  wallpaper = lib.mkDefault ../../wallpapers/reimu.png;
+  theme = lib.mkDefault ./themes/everforest-dark-hard.yaml
+}
