18d035aa95f975d6aa9b06ea4a429c931f4ac5f2
FallenGoddess7 <69827634+FallenGoddess7@users.noreply.github.com>
2025-02-26 05:57:30 -0600

    edits

--- modules/locale.nix
+++ modules/locale.nix
@@ -8,7 +8,7 @@
 # =         ########  #######   ######  ##     ## ######## ########          = #
 # ============================================================================ #
 # Localization configurations for my various NixOS systems.
-{ config, lib, pkgs, ...}: {
+{ config, lib, pkgs, systemSettings, ...}: {
   # ================================== X11 =================================== #
   services.xserver = {
     xkb.layout = lib.mkDefault "us";
@@ -16,21 +16,21 @@
   };
 
   # ================================ Timezone ================================ #
-  time.timeZone = lib.mkDefault "America/Chicago";
+  time.timeZone = lib.mkDefault systemSettings.timezone;
   time.hardwareClockInLocalTime = lib.mkDefault true; # Windows compatibility.
 
   # ===================== Internationalisation Properties ==================== #
-  i18n.defaultLocale = lib.mkDefault "en_US.UTF-8";
+  i18n.defaultLocale = lib.mkDefault systemSettings.locale;
   i18n.extraLocaleSettings = lib.mkDefault {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
+    LC_ADDRESS = systemSettings.locale;
+    LC_IDENTIFICATION = systemSettings.locale;
+    LC_MEASUREMENT = systemSettings.locale;
+    LC_MONETARY = systemSettings.locale;
+    LC_NAME = systemSettings.locale;
+    LC_NUMERIC = systemSettings.locale;
+    LC_PAPER = systemSettings.locale;
+    LC_TELEPHONE = systemSettings.locale;
+    LC_TIME = systemSettings.locale;
   };
 
   environment.variables = lib.mkDefault {
