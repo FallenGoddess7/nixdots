903b53a9d53ded46afe10017601d1807094c9940
FallenGoddess7 <69827634+FallenGoddess7@users.noreply.github.com>
2025-02-26 05:46:02 -0600

    edits

--- home/default.nix
+++ home/default.nix
@@ -1,7 +1,27 @@
-{ inputs, pkgs, lib, ... }:
+{ inputs, pkgs, lib, userSettings, ... }:
 {
+
+  home.username = userSettings.username;
+  home.homeDirectory = "/home/"+userSettings.username;
+
   imports = [
+    # ./apps/default.nix
+    ./shell/default.nix
     ./style/default.nix
+    ./wayland/default.nix
   ];
 
+
+
+
+
+
+
+
+  # What version to lock interpretation of the config file.
+  # Do not change this unless you want to rewrite most of your config files.
+  home.stateVersion = "24.11";
+
+  # Let Home-manager install and manage itself.
+  programs.home-manager.enable = true;
 }
